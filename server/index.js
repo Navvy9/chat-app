5c05a79b567d22efc8120ac720502da9b04c476a
Navvy9 <navdeep.nr2001@gmail.com>
2024-05-09 11:38:33 +0530

    origin change

--- server/index.js
+++ server/index.js
@@ -9,7 +9,8 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    
+    origin: "https://chat-app-frontend-gray.vercel.app/",
+    methods: ["GET", "POST"],
   },
 });
 
